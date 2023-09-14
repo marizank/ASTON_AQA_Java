88a51960d02fb92fc8d091cfdc5248267d7d7e22
mazank <marizankevich@gmail.com>
2023-09-14 23:42:32 +0300

    lesson_6

--- Lesson6/TaskSave.java
+++ Lesson6/TaskSave.java
@@ -6,28 +6,33 @@ import java.io.IOException;
 public class TaskSave {
 
         public static void main(String[] args) throws IOException {
-            // String[] header = { "Value 1","Value 2","Value 3",};
-            int[][] data={
-
-                    {100,200,123},
-                    {300,400,500}
-            };
             FileWriter writer=null;
             try {
                 writer = new FileWriter("test.csv");
-            /*for (int i=0; i<= header.length;i++) {
-                writer.write(String.valueOf(header[i]));
-               writer.write(" ");
-            }
-             writer.write("\r\n");*/
+                StringBuilder sb=new StringBuilder();
+                sb.append("Value1");
+                sb.append(" ");
+                sb.append("Value2");
+                sb.append(" ");
+                sb.append("Value3");
+                sb.append("\n");
 
-                for (int i = 0; i < data.length; i++) {
-                    for (int j = 0; j < data.length; j++) {
-                        writer.write(String.valueOf(data[i][j]));
-                        writer.write(" ");
-                    }
-                    writer.write("\r\n");
-                }
+                sb.append(100);
+                sb.append("    ");
+                sb.append(200);
+                sb.append("    ");
+                sb.append(123);
+                sb.append("\n");
+
+                sb.append(300);
+                sb.append("    ");
+                sb.append(400);
+                sb.append("    ");
+                sb.append(500);
+                sb.append("\n");
+
+                writer.write(sb.toString());
+
                 System.out.println("done");
             } catch (IOException e) {
                 e.printStackTrace();

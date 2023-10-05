1d58e1eb44511fa5dce9ab62ab6faa53cb0c79dd
mazank <marizankevich@gmail.com>
2023-10-05 18:40:12 +0300

    lesson_12_testng

--- src/test/java/TestFactorial.java
+++ src/test/java/TestFactorial.java
@@ -4,8 +4,18 @@ import org.testng.annotations.Test;
 
 public class TestFactorial {
     @Test
-    public void CheckFactorialEquals(){
-        Factorial nq=new Factorial();
-        Assert.assertEquals(5040, nq.factorial(7));
+    public void CheckFactorialEquals1(){
+        Factorial equal=new Factorial();
+        Assert.assertEquals(1, equal.factorial(1));
+    }
+    @Test
+    public void CheckFactorialEquals0(){
+        Factorial equal=new Factorial();
+        Assert.assertEquals(1 , equal.factorial(0));
+    }
+    @Test
+    public void CheckFactorialEquals6(){
+        Factorial equal=new Factorial();
+        Assert.assertEquals(720, equal.factorial(6));
     }
 }

844599044dcee53fec4bb6f5773b9d80c21c373a
mazank <marizankevich@gmail.com>
2023-10-10 22:31:36 +0300

    update lesson 12

---
+++ src/test/java/TestFactorial.java
@@ -0,0 +1,20 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestFactorial {
+    @Test
+    public void CheckFactorialEquals1(){
+        Factorial equal=new Factorial();
+        Assert.assertEquals(1, equal.factorial(1));
+    }
+    @Test
+    public void CheckFactorialEquals0(){
+        Factorial equal=new Factorial();
+        Assert.assertEquals(1 , equal.factorial(0));
+    }
+    @Test
+    public void CheckFactorialEquals6(){
+        Factorial equal=new Factorial();
+        Assert.assertEquals(720, equal.factorial(6));
+    }
+}
